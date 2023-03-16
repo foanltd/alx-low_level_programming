be2df85fc9b5304eed046534bc572124ce878e45
ANAS <anaselamri1234@gmail.com>
2023-03-16 14:53:28 -0700

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char Mycar[60] = "\"Programming is like building a multilingual puzzle";
-	puts(Mycar);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
