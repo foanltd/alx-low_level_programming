46c81a94aabf8ecea13797384ffa4b8364f4faa1
ANAS <anaselamri1234@gmail.com>
2023-03-17 15:27:51 -0700

    updates

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar("abcdefghijklmnopqrstuvwxyz\n");
+	return (0);
+}
