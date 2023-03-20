8a8db1af7ae6f0d9611cb86fd980bc93bcfbe1bf
ANAS <anaselamri1234@gmail.com>
2023-03-20 15:01:23 -0700

    updates

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char N = 'z';
+	for (N = 'z'; N <= 'a'; N++)
+		putchar(N);
+	return (0);
+}
