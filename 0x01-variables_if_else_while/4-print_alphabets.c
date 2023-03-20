1cc54832f3b4f74ec272ce2dfbb12b6fe7fb140e
ANAS <anaselamri1234@gmail.com>
2023-03-20 13:37:39 -0700

    updates

---
+++ 0x01-variables_if_else_while/4-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char N = 'a';
+	for (N = 'a'; N <= 'z'; N++)
+	{
+		if (N == 'q' && N == e)
+		
+			continue;
+		}
+		putchar(N);
+	       putchar('\n');
+       return (0);
+}       
